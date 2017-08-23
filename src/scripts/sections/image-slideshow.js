8d34f359f3428371b2f46d3095a1a0a833b2d6cf
Felix Tellmann <felix@tellmann.co.za>
2017-08-23 09:56:40 +0200

    Slider Section - Slate integration

---
+++ src/scripts/sections/image-slideshow.js
@@ -0,0 +1,72 @@
+/**
+ * Product Template Script
+ * ------------------------------------------------------------------------------
+ * A file that contains scripts highly couple code to the Product template.
+ *
+ * @namespace product
+ */
+
+theme.Slider = (function () {
+
+  /*var selectors = {
+   addToCart: '[data-add-to-cart]',
+   addToCartText: '[data-add-to-cart-text]',
+   comparePrice: '[data-compare-price]',
+   comparePriceText: '[data-compare-text]',
+   originalSelectorId: '[data-product-select]',
+   priceWrapper: '[data-price-wrapper]',
+   productFeaturedImage: '[data-product-featured-image]',
+   productJson: '[data-product-json]',
+   productPrice: '[data-product-price]',
+   productThumbs: '[data-product-single-thumbnail]',
+   singleOptionSelector: '[data-single-option-selector]'
+   };*/
+
+  console.log('test');
+  /**
+   * Product section constructor. Runs on page load as well as Theme Editor
+   * `section:load` events.
+   * @param {string} container - selector for the section container DOM element
+   */
+  function Slider(container) {
+    this.$container = $(container);
+    this.settings = {};
+    this.namespace = '.slider';
+
+    var sectionId = this.$container.attr('data-section-id');
+
+    var portfolioSelectors = {
+      sliderContainer: $('.slider').attr('data-section-id', sectionId)
+    };
+
+
+
+    this.initSlider(portfolioSelectors);
+  }
+
+  Slider.prototype = $.extend({}, Slider.prototype, {
+
+    initSlider: function (sliderContainer) {
+      sliderContainer.sliderContainer.slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        prevArrow: false,
+        nextArrow: false
+      });
+
+    },
+
+
+    onBlockSelect: function (evt) {
+      this.$container.off(this.namespace);
+
+      var slideIndex = $('.slick-slide[data-block-id="' + evt.detail.blockId + '"]:not(.slick-cloned)').attr('data-slick-index');
+      $('.slider').attr('data-section-id', evt.detail.sectionId).slick('slickGoTo', slideIndex)
+
+    }
+
+  });
+
+  return Slider;
+})();
